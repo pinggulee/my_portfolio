3f5bf6496ce265abc66695445e95b9e647a1cfdc
pinggulee <gusrn11204@gmail.com>
2021-10-07 17:45:15 +0900

    21/10/07

--- ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
+++ ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
@@ -24,7 +24,7 @@ public class TimeMapperTests {
 		log.info(timeMapper.getClass().getName());
 		log.info(timeMapper.getTime());
 	}
-	
+
 	@Test
 	public void testGetTime2() {
 		log.info("getTime2");
